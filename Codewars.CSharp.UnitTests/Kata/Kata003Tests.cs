ca4f81dce516ab80093dc2c40e3ad2fbd766fb08
Revolt27 <kuckushckin.alex@yandex.ru>
2020-07-11 15:52:20 +0300

    C# Kata - "Multiples of 3 or 5"

---
+++ Codewars.CSharp.UnitTests/Kata/Kata003Tests.cs
@@ -0,0 +1,21 @@
+﻿using Codewars.CSharp.Kata;
+using NUnit.Framework;
+
+namespace Codewars.CSharp.UnitTests.Kata
+{
+    [TestFixture]
+    public class Kata003Tests
+    {
+        [TestCase(0, 0)]
+        [TestCase(3, 4)]
+        [TestCase(8, 6)]
+        [TestCase(23, 10)]
+        [TestCase(78, 20)]
+
+        public void Test(int expected, int number)
+        {
+            var actual = Kata003.Solution(number);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Codewars.CSharp/Kata/Kata003.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+ *
+ * Note: If the number is a multiple of both 3 and 5, only count it once.
+ */
+namespace Codewars.CSharp.Kata
+{
+    public static class Kata003
+    {
+        /// <summary>
+        /// My version.
+        /// </summary>
+        public static int Solution(int value)
+        {
+            var result = new List<int>();
+
+            for (var i = 1; i < Math.Ceiling(value / 3.0d); i++)
+            {
+                var numbers = new List<int> { (3 * i), (5 * i) };
+
+                result.AddRange(numbers.Where(num => num < value && !result.Contains(num)));
+            }
+
+            return result.Sum();
+        }
+
+        /// <summary>
+        /// Best practice according to the community Codewars.
+        /// </summary>
+        public static int SolutionBestPractice(int n)
+        {
+            return Enumerable.Range(0, n).Where(e => e % 3 == 0 || e % 5 == 0).Sum();
+        }
+    }
+}
