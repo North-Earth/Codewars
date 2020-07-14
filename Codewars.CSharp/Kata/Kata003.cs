dd311ce0719e242209399ff272129600aa4e1100
Revolt27 <kuckushckin.alex@yandex.ru>
2020-07-14 22:36:16 +0300

    Added comment in Kata003

--- Codewars.CSharp/Kata/Kata003.cs
+++ Codewars.CSharp/Kata/Kata003.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 
-/*
- * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
- * Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
- *
- * Note: If the number is a multiple of both 3 and 5, only count it once.
- */
 namespace Codewars.CSharp.Kata
 {
+    /*
+     * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+     * Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+     *
+     * Note: If the number is a multiple of both 3 and 5, only count it once.
+     */
     public static class Kata003
     {
+        /*
+         * Thoughts:
+         * Very not optimal code :(
+         */
+
         /// <summary>
         /// My version.
         /// </summary>
