abb3632a4bfde3577d620c92e171048145ea0f0e
Revolt27 <kuckushckin.alex@yandex.ru>
2020-07-14 23:30:42 +0300

    C# Kata - "Detect Pangram"

---
+++ Codewars.CSharp.UnitTests/Kata/Kata005Tests.cs
@@ -0,0 +1,15 @@
+﻿using Codewars.CSharp.Kata;
+using NUnit.Framework;
+
+namespace Codewars.CSharp.UnitTests.Kata
+{
+    [TestFixture]
+    public class Kata005Tests
+    {
+        [Test]
+        public void SampleTests()
+        {
+            Assert.AreEqual(true, Kata005.IsPangram("The quick brown fox jumps over the lazy dog."));
+        }
+    }
+}
