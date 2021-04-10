e7ddd4ad107aebb621bb1c0f074848adec081235
Aleksey Kukushkin <Kuckushckin.alex@yandex.ru>
2021-04-10 20:30:04 +0300

    C# Kata - "Rot13"

---
+++ Codewars.CSharp.UnitTests/Kata/Kata009Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Codewars.CSharp.Kata;
+using NUnit.Framework;
+
+namespace Codewars.CSharp.UnitTests.Kata
+{
+    [TestFixture]
+    public class Kata009Tests
+    {
+        [Test, Description("test")]
+        public void testTest()
+        {
+            Assert.AreEqual("grfg", Kata009.Rot13("test"), String.Format("Input: test, Expected Output: grfg, Actual Output: {0}", Kata009.Rot13("test")));
+        }
+
+        [Test, Description("Test")]
+        public void TestTest()
+        {
+            Assert.AreEqual("Grfg", Kata009.Rot13("Test"), String.Format("Input: Test, Expected Output: Grfg, Actual Output: {0}", Kata009.Rot13("Test")));
+        }
+    }
+}
