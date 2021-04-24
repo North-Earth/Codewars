214632ce61a529a1af60847e87099f73e2a6d212
Aleksey Kukushkin <Kuckushckin.alex@yandex.ru>
2021-04-24 23:50:30 +0300

    C# Kata - "Human Readable Time"

---
+++ Codewars.CSharp.UnitTests/Kata/Kata012Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Codewars.CSharp.Kata;
+using NUnit.Framework;
+
+namespace Codewars.CSharp.UnitTests.Kata
+{
+    [TestFixture]
+    public class Kata012Tests
+    {
+        [Test]
+        public void HumanReadableTest()
+        {
+            Assert.AreEqual("00:00:00", Kata012.GetReadableTime(0));
+            Assert.AreEqual("00:00:05", Kata012.GetReadableTime(5));
+            Assert.AreEqual("00:01:00", Kata012.GetReadableTime(60));
+            Assert.AreEqual("23:59:59", Kata012.GetReadableTime(86399));
+            Assert.AreEqual("99:59:59", Kata012.GetReadableTime(359999));
+        }
+    }
+}
---
+++ Codewars.CSharp/Kata/Kata012.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Codewars.CSharp.Kata
+{
+    /// <summary>
+    /// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+    /// HH = hours, padded to 2 digits, range: 00 - 99
+    /// MM = minutes, padded to 2 digits, range: 00 - 59
+    /// SS = seconds, padded to 2 digits, range: 00 - 59
+    /// The maximum time never exceeds 359999 (99:59:59)
+    /// </summary>
+    public static class Kata012
+    {
+        /// <summary>
+        /// My version.
+        /// </summary>
+        public static string GetReadableTime(int seconds)
+            => $"{GetReadableNumber(Math.DivRem(seconds, 3600, out int remainder).ToString())}" +
+               $":{GetReadableNumber(Math.DivRem(remainder, 60, out remainder).ToString())}" +
+               $":{GetReadableNumber(remainder.ToString())}";
+
+        private static string GetReadableNumber(string number)
+            => $"{(number.Length < 2 ? "0" + number : number)}";
+
+        #region Best practice according to the community Codewars.
+
+        public static string GetReadableTimeBestPractice1(int seconds)
+        {
+            return string.Format("{0:d2}:{1:d2}:{2:d2}", seconds / 3600, seconds / 60 % 60, seconds % 60);
+        }
+
+        public static string GetReadableTimeBestPractice2(int seconds)
+        {
+            var t = TimeSpan.FromSeconds(seconds);
+            return string.Format("{0:00}:{1:00}:{2:00}", (int)t.TotalHours, t.Minutes, t.Seconds);
+        }
+
+        #endregion
+    }
+}
